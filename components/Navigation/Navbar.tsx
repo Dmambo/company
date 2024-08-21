ab23b11472d5f643f52fd739a19d862f0a78b63b
Dmambo <mambotrader0@gmail.com>
2024-08-22 02:13:21 +0530

    Added glossy feel to the navigation

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -3,10 +3,9 @@
 import { useState } from "react";
 import Link from "next/link";
 import UpperNav from "./NavComponents/UpperPart";
-import { Phone, Menu, X, ChevronDown } from "lucide-react";
+import { Phone, Menu, X } from "lucide-react";
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
-import Image from "next/image";
 import Logo from "../logo";
 
 const Navbar = () => {
@@ -27,11 +26,9 @@ const Navbar = () => {
       </div>
       <Separator />
       {/* Main Navigation Bar */}
-      <div className="flex justify-between items-center p-4 bg-black text-white relative ">
-        <div className="text-red-600 text-2xl md:text-3xl ">
-       
+      <div className="sticky top-0 z-50 flex justify-between items-center p-4 bg-black/60 backdrop-blur-lg text-white">
+        <div className="text-red-600 text-2xl md:text-3xl">
           <Logo />
-
         </div>
 
         {/* Menu toggle button for mobile */}
@@ -45,12 +42,12 @@ const Navbar = () => {
         <div
           className={`${
             menuOpen ? "block" : "hidden"
-          } md:flex md:gap-10 md:items-center md:w-auto flex flex-col md:flex-row absolute md:static top-16 left-0 w-full bg-black md:bg-transparent z-40`}
+          } md:flex md:gap-10 md:items-center md:w-auto flex flex-col md:flex-row absolute md:static top-16 left-0 w-full bg-black/80 md:bg-transparent z-40`}
         >
           <div className="md:hidden my-3"></div>
           <Link
             href="/"
-            className="hover:text-green-600 py-8 px-6 md:py-0"
+            className="hover:text-green-600 py-8 px-6 md:py-0 hover:underline transition duration-300 ease-in-out"
             onClick={closeMenu}
           >
             Home
@@ -59,7 +56,7 @@ const Navbar = () => {
           {/* About Us Dropdown */}
           <Link
             href="/about"
-            className="hover:text-green-600 py-8 px-6 md:py-0"
+            className="hover:text-green-600 py-8 px-6 md:py-0 hover:underline transition duration-300 ease-in-out"
             onClick={closeMenu}
           >
             About Us
@@ -68,7 +65,7 @@ const Navbar = () => {
           {/* Services Dropdown */}
           <Link
             href="/services"
-            className="hover:text-green-600 py-8 px-6 md:py-0"
+            className="hover:text-green-600 py-8 px-6 md:py-0 hover:underline transition duration-300 ease-in-out"
             onClick={closeMenu}
           >
             Services
@@ -77,16 +74,15 @@ const Navbar = () => {
           {/* Portfolio */}
           <Link
             href="/portfolio"
-            className="hover:text-green-600 py-8 px-6 md:py-0"
+            className="hover:text-green-600 py-8 px-6 md:py-0 hover:underline transition duration-300 ease-in-out"
             onClick={closeMenu}
           >
             Portfolio
           </Link>
 
-    
           <Link
             href="/contact"
-            className="hover:text-green-600 py-8 px-6 md:py-0 flex items-center"
+            className="hover:text-green-600 py-8 px-6 md:py-0 flex items-center hover:underline transition duration-300 ease-in-out"
             onClick={closeMenu}
           >
             Contact Us
@@ -94,10 +90,10 @@ const Navbar = () => {
 
           {/* Call Us Button */}
           <div className="mt-4 md:mt-0 mb-2 flex justify-center items-center p-4">
-                        <Button variant={'outline'} className='bg-black border-green-500 text-blue-600'>
-                            <Phone className="mr-2 text-blue-600" /> Let&apos;s Talk
-                        </Button>
-                    </div>
+            <Button variant={'outline'} className='bg-black border-green-500 text-blue-600'>
+              <Phone className="mr-2 text-blue-600" /> Let&apos;s Talk
+            </Button>
+          </div>
         </div>
       </div>
     </>
